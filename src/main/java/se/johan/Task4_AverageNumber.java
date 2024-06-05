5ce84ff087b0bead8d5816579c4c48c30f57fa87
Johan-Danielsson <johan.dunhill@gmail.com>
2024-06-05 13:34:28 +0200

    Added Task4

---
+++ src/main/java/se/johan/Task4_AverageNumber.java
@@ -0,0 +1,15 @@
+package se.johan;
+
+public class Task4_AverageNumber {
+    public static void main(String[] args) {
+        //change values of num1, num2, num3 to generate your
+        // desired average result
+
+        int num1 = 23;
+        int num2 = 11;
+        int num3 = 77;
+        int sum = num1 + num2 + num3;
+        int average = sum / 3;
+        System.out.println("("+num1+ "+" +num2+ "+" +num3+") /3 ="+ average);
+    }
+}
