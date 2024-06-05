7da46ceab64b1c712fcb533aa03fee9b47d186d5
Johan-Danielsson <johan.dunhill@gmail.com>
2024-06-05 14:13:29 +0200

    Added Task5

---
+++ src/main/java/se/johan/Task4_HelloUserNameInput.java
@@ -0,0 +1,16 @@
+package se.johan;
+
+import java.util.Scanner;
+
+public class Task4_HelloUserNameInput {
+    public static void main(String[] args) {
+
+        Scanner name = new Scanner(System.in);
+
+        System.out.println("What is your name?");
+
+        String yourName = name.nextLine();
+
+        System.out.println("Hello "+ yourName + "!");
+    }
+}
