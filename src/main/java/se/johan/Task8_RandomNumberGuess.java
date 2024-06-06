d9ee1523235fa45481c915d4c29ee1cc36c06a0a
Johan-Danielsson <johan.dunhill@gmail.com>
2024-06-06 15:21:41 +0200

    Added Task 8, program that generates random number and lets you guess, with attempt counter

--- src/main/java/se/johan/Task8_RandomNumberGuess.java
+++ src/main/java/se/johan/Task8_RandomNumberGuess.java
@@ -9,6 +9,8 @@ public class Task8_RandomNumberGuess {
         // It will then let you guess what number it is, and tell you
         // if your guess is lower or higher than the generated number.
         // It will keep on going until you guess the correct number.
+        // Once you hit the right number it will also tell you how many
+        // attempts it took until you found it.
 
         //Adding Random class to generate a random number
         Random randomNumber = new Random();
@@ -16,7 +18,7 @@ public class Task8_RandomNumberGuess {
         // Generate a random number between 1 and 500
         int randomNumber1 = randomNumber.nextInt(499);
 
-        //  print the random number for reference while making the code
+        //  Print the random number for reference while making the code
         // System.out.println(randomNumber1);
 
         // Adding Scanner to take the guess as input
@@ -37,7 +39,7 @@ public class Task8_RandomNumberGuess {
             // if the guess is correct, congratulate and end program
             if (guess == randomNumber1) {
                 System.out.println("Correct!");
-                System.out.println("You got there in " +attempts +" guesses!");
+                System.out.println("You got there in " +attempts +" attempts!");
                 System.exit(0);
             }
             // if the guess is >= (smaller) inform user and run again
